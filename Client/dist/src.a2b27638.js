685c4f13cc4a2e0c0a3b3389eea8d9df9e940a8e
NishanNiriella <nishandilip12@gmail.com>
2021-06-25 20:01:11 +0530

    taking_pull1_20210625

--- Client/dist/src.a2b27638.js
+++ Client/dist/src.a2b27638.js
@@ -47781,7 +47781,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54467" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62184" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
